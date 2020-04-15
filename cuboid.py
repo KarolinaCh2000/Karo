44742555cad2dc22a4b660a80b1897b3a78431ad
KarolinaCh2000 <62103705+KarolinaCh2000@users.noreply.github.com>
2020-04-15 17:04:39 +0200

    Add files via upload

---
+++ cuboid.py
@@ -0,0 +1,7 @@
+a=float(input("the 1 of base edge lenght is: "))
+b=float(input("the 2 of base edge lenght is : "))
+h=float(input("the cuboid's height is : "))
+surface_area=(2*a*b)+(2*a*h)+(2*b*h)
+volume=a*b*h
+print("The surface area of the cuboid is: ",surface_area )
+print("The cuboid volume is: ", volume)
